ae760fba69da9f627c327a220e70fa1d944eb18a
kreeyuh77 <57364671+kreeyuh77@users.noreply.github.com>
2021-07-01 22:19:24 -0400

    Add files via upload

---
+++ frontend/desktop/pages/Events.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+
+function Events() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Events!</Text>
+    </View>
+  );
+}
+
+export default Events;
