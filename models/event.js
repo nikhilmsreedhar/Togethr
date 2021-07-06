8ef6efcf8e7415de0acbded36e84eadbe5694399
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-07-06 16:20:23 -0400

    Create event.js

---
+++ models/event.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema({
+    Title: {
+        type: String, 
+        required: true
+    },
+    Description: {
+        type: String, 
+        required: true
+    },
+    Location: {
+        type: String, 
+        required: true
+    },
+    Time: {
+        type: String, 
+        required: true
+    },
+    NumOfpeople: {
+        type: Number, 
+        required: true
+    },
+    Picture: {
+        type: String,
+        required: false
+    },
+});
+module.exports = Event = mongoose.model("Events", EventSchema);
