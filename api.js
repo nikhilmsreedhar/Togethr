cff481657496483a6f5d374edf3fe65db235b335
Anthonyjackson <56099042+Anthonyjackson@users.noreply.github.com>
2021-07-06 13:20:01 -0400

    Add files via upload

---
+++ api.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const User = require("./models/user.js");
+const router = express.Router();
+
+
+exports.setApp = function ( app, client )
+{
+app.post('/api/adduser', async (req, res, next) => {
+
+  const{ UserName,
+    Password, FirstName, LastName, DOB, Picture, Rating,Email, Tags} = req.body;
+
+  let user = new User({
+    UserName,
+    Password,
+    FirstName,
+    LastName,
+    DOB,
+    Picture,
+    Rating,
+    Email,
+    Tags
+  })
+
+  try{
+    user = await user.save();
+    res.send(user);
+  }catch (error) {
+    res.status(500).send(error.message);
+    console.log(error.message);
+  }
+
+});
+
+app.post('/api/login', async (req, res, next) => {
+  const UserName = req.body.UserName;
+  const Password = req.body.Password;
+  
+
+  User.findOne({ UserName })
+    .then(user => {
+      if (!user)
+        return res.status(301).json({ warning: "Incorrect Credentials" });
+
+      User.findOne({ Password })
+        .then(same => {
+          if (!same)
+            return res.status(301).json({ warning: "Incorrect Credentials" });
+
+          res.json({
+            id: user.id,
+            UserName: user.UserName,
+            FirstName: user.FirstName,
+            LastName: user.LastName
+          });
+        })
+        .catch(err => res.status(400).json("Error" + err));
+    })
+    .catch(err => res.status(400).json("Error" + err));
+});
+
+
+
+
+module.exports = router
+}
---
+++ server.js
@@ -0,0 +1,53 @@
+var compression = require('compression');
+var express = require('express');
+var cors = require('cors');
+var helmet = require('helmet');
+var mongoose = require('mongoose');
+var path = require('path');
+
+require('dotenv').config();
+
+
+// Create the express server
+var app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(compression());
+app.use(cors());
+
+
+
+const URI = process.env.MONGODB_URI;
+// Connect to MongoDB Atlas
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB Atlas connection established successfully'))
+  .catch(err => console.log(err));
+  const users = require('./api');
+
+  users.setApp( app, mongoose );
+  
+  app.get("/", (req,res) => {
+    res.send("welcome to our add user api")
+  })
+
+
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
