ee530714556dcbe32d0733820ff47114c06061db
kreeyuh77 <57364671+kreeyuh77@users.noreply.github.com>
2021-07-06 13:47:54 -0400

    Add files via upload

--- frontend/desktop/pages/RegisterPage.js
+++ frontend/desktop/pages/RegisterPage.js
@@ -8,11 +8,9 @@ import {
 } from '@expo-google-fonts/dev';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
-import { TextInput, HelperText} from 'react-native-paper';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
-import TextField from '@material-ui/core/TextField';
+import { TextInput} from 'react-native-paper';
+
+
 
 
 
@@ -23,59 +21,29 @@ export default function RegisterPage() {
     Roboto_700Bold
   });
 
-  const theme = createMuiTheme({
-    palette: {
-      secondary: {
-        main: '#5b06d5'
-      }
-    }
-  });
-
   
   const navigation = useNavigation();
   function navigateBack() {
     navigation.goBack();
 }
 
-function goToNextPage(fname, lname, bday){
-  if (fname == "" || lname == "" || bday == "") {
+function goToNextPage(fname, lname){
+  if (fname == "" || lname == "") {
     alert('Please fill in all fields') ;
   }
   else{
-   navigation.navigate('RegisterPage2', {firstName: fname, lastName: lname, birthday: bday});
+    navigation.navigate('RegisterPage2', {firstName: fname, lastName: lname});
   }
 }
-
-// Caclulate users age to make sure they are 18
-function calculate_age(dob1){
-  var today = new Date();
-  var birthDate = new Date(dob1);  
-  var age_now = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
-  {
-      age_now--;
-  }
-  console.log(age_now);
-  return age_now;
-}
-
-const over18 = (dob1) => {
-  return !(calculate_age(dob1)>=18);
-};
 
 
 const [fname, setFirst] = React.useState('');
 const [lname, setLast] = React.useState('');
-const [birthday, setBirthday] = React.useState(new Date());
 
-const handleFirstChange = (event) => {
-  setFirst(event.target.value);
-};
-const handleLastChange = (event) => {
-  setLast(event.target.value);
-};
 
+const next = (fname, lname) => {
+  alert('First: ' + fname + ' Last: '+ lname);
+};
 
   return (
     <SafeAreaView style={{flex: 1}}>
@@ -84,66 +52,33 @@ const handleLastChange = (event) => {
     <TouchableOpacity onPress={() => navigateBack()}>
       <Ionicons name="arrow-back"  size={30} color="back" />
     </TouchableOpacity>
-    <View style={styles.center}>
+    <Text style={styles.verticalDivider}></Text>
         <Text h1 style={styles.title}>Register</Text>
         <Text style={styles.verticalDivider}></Text>
-          
-          <MuiThemeProvider theme={theme}>
-          <TextField 
-          style={{width: 500}}
-          color = 'secondary'
-          label="First Name" 
-          variant="outlined" 
+        
+        <TextInput style={{ alignSelf: 'stretch'}}
+          label="First Name"
           value={fname}
-          onChange={handleFirstChange}
-          />
-          
-          <Text Text style={styles.inputDivider}></Text>
+          mode='outlined'
+          onChangeText={fname => setFirst(fname)}
+        />
 
-          <TextField 
-          style={{width: 500}}
-          color = 'secondary'
-          label="Last Name" 
-          variant="outlined" 
+        <Text style={styles.inputDivider}></Text>
+
+        <TextInput style={{ alignSelf: 'stretch'}}
+          label="Last Name"
           value={lname}
-          onChange={handleLastChange}
-          />
-
-          <Text Text style={styles.inputDivider}></Text>
-
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <KeyboardDatePicker
-            autoOk
-            color='secondary'
-            variant="inline"
-            inputVariant="outlined"
-            label="Birthday"
-            format="MM/dd/yyyy"
-            value={birthday}
-            KeyboardButtonProps={{
-              'aria-label': 'change date',
-            }}
-            onChange={birthday => setBirthday(birthday)}
-          />
-          </MuiPickersUtilsProvider>
-          <HelperText type="error" visible={over18(birthday)}>
-            Must be over 18 to crate an account
-          </HelperText>
-          </MuiThemeProvider>
+          mode='outlined'
+          onChangeText={lname => setLast(lname)}
+         />
 
           <Text style={styles.inputDivider}></Text>
-         <View style={styles.fixToText}>
-        <TouchableOpacity onPress={()=>navigateBack()} style={styles.backButton}>
-          <Text style={styles.backButtonText}>CANCEL</Text>
-       </TouchableOpacity>
-       <Text style={styles.buttonDivider}></Text>
-       <TouchableOpacity disabled = {(calculate_age(birthday) >= 18) ? false : true} onPress={() => goToNextPage(fname, lname, birthday)} style={styles.nextButton}>
-          <Text style={styles.nextButtonText}>NEXT</Text>
-       </TouchableOpacity>
-        </View>
-       
 
-        </View>
+         <TouchableOpacity  onPress={()=>{goToNextPage(fname, lname)}} style={styles.loginButton}>
+          <Text style={styles.loginButtonText}>NEXT</Text>
+         </TouchableOpacity>
+
+        
    </View>
   
     
@@ -161,7 +96,8 @@ const styles = StyleSheet.create({
     fontFamily: 'Comfortaa_400Regular',
   },
   input:{
-    width: 500,
+    padding: 20, 
+  
  },
   container: {
     flex: 1,
@@ -170,55 +106,26 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
     margin: 25
   },
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignSelf: 'center'
-  },
    verticalDivider: {
-    height:25,
+    height:50,
   },
   inputDivider: {
     height:20,
   },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  nextButton: {
+  loginButton: {
     backgroundColor: "black",
     borderColor:"black",
     alignSelf: 'stretch',
     height:50,
-    width: 245,
     borderWidth:3,
     alignItems:'center',
     justifyContent:'center',
     borderRadius: 5,
   },
-  nextButtonText: {
+  loginButtonText: {
     fontSize: 18,
     color: 'white',
     fontFamily: 'Roboto_500Medium'
   }, 
-  backButton: {
-    backgroundColor: "grey",
-    borderColor:"grey",
-    width:245,
-    height:50,
-    borderWidth:3,
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius: 5,
-  },
-  backButtonText: {
-    fontSize: 18,
-    color: 'black',
-    fontFamily: 'Roboto_500Medium'
-  }, 
-  buttonDivider: {
-    width:10,
-  },
 });
   
