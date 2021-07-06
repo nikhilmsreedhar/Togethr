72a0ca8c972ae3f3ed8ad86263df733c78e3a07a
Anthonyjackson <56099042+Anthonyjackson@users.noreply.github.com>
2021-07-06 13:19:18 -0400

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// var Int32 = require('mongoose-int32');
+//Create Schema
+const UserSchema = new Schema({
+  // _id: {
+  //   type: Number
+  // },
+  UserName: {
+    type: String,
+    required: true
+  },
+  Password: {
+    type: String,
+    required: true
+  },
+  FirstName: {
+    type: String,
+    required: true
+  },
+  LastName: {
+    type: String,
+    required: true
+  },
+  DOB: {
+    type: String,
+    required: false
+  },
+  Picture: {
+    type: String,
+    required: false
+  },
+  Rating: {
+    type: Number,
+    required: false
+  },
+  Email: {
+    type: String,
+    required: true
+  },
+  Tags: {
+    type: Array,
+    required: false
+  }
+});
+module.exports = User = mongoose.model("Users", UserSchema);
