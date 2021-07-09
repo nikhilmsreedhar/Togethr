6d9bec648800486b74ef4c00513b29adf62b231a
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-07-09 15:02:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ mongoose
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
