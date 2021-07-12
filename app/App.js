a06b9d42d0afbb379c72f5294f347b15ced5a9c0
Michael Negre <michael.nehgrei@gmail.com>
2021-07-12 15:02:30 -0400

    implement Provider for app themes

--- app/App.js
+++ app/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -8,19 +9,23 @@ import  WelcomeScreen from './screens/WelcomeScreen'
 import  LoginScreen from './screens/LoginScreen'
 import  RegisterScreen from './screens/RegisterScreen'
 import  RegisterScreen2 from './screens/RegisterScreen2'
+import Main from './containers/Main'
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="WelcomeScreen" headerMode="none">
-        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        <Stack.Screen name="RegisterScreen2" component={RegisterScreen2} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    //PaperProvider handles theming from React Native Paper
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="WelcomeScreen" headerMode="none">
+          <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="RegisterScreen2" component={RegisterScreen2} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
