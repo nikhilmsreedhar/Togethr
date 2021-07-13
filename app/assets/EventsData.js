2ca986398d576974e5bbb35f233b721663fce6a1
Michael Negre <michael.nehgrei@gmail.com>
2021-07-13 15:32:00 -0400

    Example data packages for Events and User

---
+++ app/assets/EventsData.js
@@ -0,0 +1,31 @@
+module.exports = [
+	{
+        id: 1,
+        title:'Tite',
+        description:'Description',
+        date:'MM/DD/YYYY',
+        startTime:'0:00pm',
+        endTime:'0:00pm',
+        attendees:'0/0',
+    },
+    {
+        id: 2,
+        title:'Let\'s go Shopping',
+        description:'Come shopping with me at the Florida Mall',
+        date:'10/09/2020',
+        startTime:'4:00pm',
+        endTime:'5:00pm',
+        attendees:'0/4',
+    },
+    {
+        id: 3,
+        title:'Puppy Playdate',
+        description:'My corgi needs a friend, meet me at the park ',
+        date:'10/09/2020',
+        startTime:'4:00pm',
+        endTime:'5:00pm',
+        attendees:'0/4',
+    }
+    
+
+];
---
+++ app/assets/UserData.js
@@ -0,0 +1,8 @@
+module.exports = {
+    id: 0,
+    firstname: "John",
+    lastname: "Doe",
+    tags: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    events: [],
+    liked: [],
+};
