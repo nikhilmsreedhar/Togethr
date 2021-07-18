3c054009471a63917f720ca8d55af7333b99b0cf
kreeyuh77 <57364671+kreeyuh77@users.noreply.github.com>
2021-07-18 13:19:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ mongoose
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/desktop/build'));
+  app.use(express.static('frontend/desktop/public/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'desktop', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'desktop', 'public', 'build', 'index.html'));
   });
 }
 
