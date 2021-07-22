0ae3ec8afba9ae6435681bc15c1219b477d101b7
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-07-22 14:18:02 -0400

    Update api.js

--- api.js
+++ api.js
@@ -172,7 +172,7 @@ app.post('/api/login', async (req, res, next) => {
 
 
 app.post('/api/addevent', async (req, res, next) => {
-  const{ Maker, EventName, EventDescription, EventLocation, StartDate, EndDate, Attendees, Pictures, Tag} = req.body;
+  const{ Maker, EventName, EventDescription, EventLocation, StartDate, EndDate, NumGuests, Attendees, Pictures, Tag} = req.body;
 
   let event = new Event({
     Maker,
@@ -181,6 +181,7 @@ app.post('/api/addevent', async (req, res, next) => {
     EventLocation,
     StartDate,
     EndDate,
+    NumGuests,
     Attendees,
     Pictures, 
     Tag
