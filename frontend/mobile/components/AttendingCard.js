bdfb95770510d4cef6f07da116db5ced535f4b22
Michael Negre <michael.nehgrei@gmail.com>
2021-07-23 12:04:50 -0400

    rename EventCard -> AttendingCard

---
+++ frontend/mobile/components/AttendingCard.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {View, StyleSheet, Text,Dimensions} from 'react-native';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+
+
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
+
+const AttendingCard = ({
+  title, 
+  description,
+  date,
+  startTime,
+  endTime,
+  attendees,
+}) => {
+    return (
+      <Card>
+        <Card.Content>
+          <Title>{title}</Title>
+          <Paragraph>{description}</Paragraph>
+        </Card.Content>
+        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        <Card.Actions>
+          <Button>Cancel</Button>
+          <Button>Ok</Button>
+        </Card.Actions>
+      </Card>
+   );
+}
+ 
+
+const styles = StyleSheet.create({
+  containerCardItem: {
+  height: SCREEN_HEIGHT - 160,
+  width: SCREEN_WIDTH - 30,
+  paddingVertical: 50,
+  backgroundColor: 'white',
+  borderRadius: 8,
+  alignItems: "center",
+  justifyContent: 'center',
+  shadowOpacity: 0.05,
+  shadowRadius: 10,
+  shadowColor: 'black',
+  shadowOffset: { height: 0, width: 0 }
+  }, 
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  description: {
+    fontSize: 20,
+    margin: 10,
+  },
+  info: {
+    margin: 10,
+  }
+});
+  
+export default AttendingCard;
