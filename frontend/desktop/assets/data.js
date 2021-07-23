6141d39ed48f348ee2e14d99b639dcb0773f7b90
Colin <zenarux@gmail.com>
2021-07-23 16:47:01 -0400

    y

--- frontend/desktop/assets/data.js
+++ frontend/desktop/assets/data.js
@@ -1,157 +1,92 @@
-function retrieve() {
-axios.post('https://togethrgroup1.herokuapp.com/api/retrieveevents', {
-    Tags: tags
-    })
-    .then((response) => {
-        console.log(response);
-        var events = response.data;
-    }, (error) => {
-        console.log(error);   
-    });
-}
 module.exports = [
+    {
+        title:'Family Time',
+        description:'Family Bonding',
+        location: 'Location',
+        startDate: new Date().toString(),
+        endDate: new Date().toString(),
+        guests: '4',
+        attendees:["jon", "Sansa", "Arya", "Robb", "Bran"],
+        tag:'DIY'
+    },
     {
         title:'Title',
         description:'Description',
-<<<<<<< HEAD
         location: 'Location',
-        startTime:'MM/DD/YYYY 0:00pm',
-        endTime:'MM/DD/YYYY 0:00pm',
-        guests:'0/0',
+        startDate:'MM/DD/YYYY 0:00pm',
+        endDate:'MM/DD/YYYY 0:00pm',
+        guests: 0,
+        attendees:'Me',
+        tag:'DIY'
+    },
+    {
+        title:'Title',
+        description:'Description',
+        location: 'Location',
+        startDate:'MM/DD/YYYY 0:00pm',
+        endDate:'MM/DD/YYYY 0:00pm',
+        guests: 0,
+        attendees:'Me',
+        tag:'DIY'
+    },
+    {
+        title:'Title',
+        description:'Description',
+        location: 'Location',
+        startDate:'MM/DD/YYYY 0:00pm',
+        endDate:'MM/DD/YYYY 0:00pm',
+        guests: 0,
+        attendees:'Me',
+        tag:'DIY'
+    },
+    {
+        title:'Title',
+        description:'Description',
+        location: 'Location',
+        startDate:'MM/DD/YYYY 0:00pm',
+        endDate:'MM/DD/YYYY 0:00pm',
+        guests: 0,
+        attendees:'Me',
+        tag:'DIY'
+    },
+    {
+        title:'Title',
+        description:'Description',
+        location: 'Location',
+        startDate:'MM/DD/YYYY 0:00pm',
+        endDate:'MM/DD/YYYY 0:00pm',
+        guests: 0,
+        attendees:'Me',
+        tag:'DIY'
+    },
+    {
+        title:'Title',
+        description:'Description',
+        location: 'Location',
+        startDate:'MM/DD/YYYY 0:00pm',
+        endDate:'MM/DD/YYYY 0:00pm',
+        guests: 0,
+        attendees:'Me',
+        tag:'DIY'
+    },
+    {
+        title:'Title',
+        description:'Description',
+        location: 'Location',
+        startDate:'MM/DD/YYYY 0:00pm',
+        endDate:'MM/DD/YYYY 0:00pm',
+        guests: 0,
+        attendees:'Me',
+        tag:'DIY'
+    },
+    {
+        title:'Title',
+        description:'Description',
+        location: 'Location',
+        startDate:'MM/DD/YYYY 0:00pm',
+        endDate:'MM/DD/YYYY 0:00pm',
+        guests: 0,
         attendees:'Me',
         tag:'DIY'
     }
-=======
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Let\'s go Shopping',
-        description:'Come shopping with me at the Florida Mall',
-        date:'10/09/2020',
-        startTime:'4:00pm',
-        endTime:'5:00pm',
-        atendees:'0/4',
-    },
-    {
-        title:'Puppy Playdate',
-        description:'My corgi needs a friend, meet me at the park ',
-        date:'10/09/2020',
-        startTime:'4:00pm',
-        endTime:'5:00pm',
-        atendees:'0/4',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },  {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },  {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
-    {
-        title:'Title',
-        description:'Description',
-        date:'MM/DD/YYYY',
-        startTime:'0:00pm',
-        endTime:'0:00pm',
-        atendees:'0/0',
-    },
->>>>>>> 4ec26dbc0fb52a9e64ddf23e403fe87df206e7b5
 ];
--- frontend/desktop/pages/Events.js
+++ frontend/desktop/pages/Events.js
@@ -22,10 +22,9 @@ function Events() {
         <Card 
          title={item.title}
          description={item.description}
-         date={item.date}
-         startTime={item.startTime}
-         endTime={item.endTime}
-         atendees={item.atendees}/>
+         startDate={item.startDate}
+         endDate={item.endDate}
+         attendees={item.attendees}/>
       
     </Accordion> 
     ))}
