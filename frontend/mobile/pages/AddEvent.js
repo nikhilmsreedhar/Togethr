aab7f0ce8616ff13a5ba70a7cfe5e273f9eb041a
Michael Negre <michael.nehgrei@gmail.com>
2021-07-25 02:09:51 -0400

    add AddEvent styling

--- frontend/mobile/pages/AddEvent.js
+++ frontend/mobile/pages/AddEvent.js
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
   Text,
   TouchableOpacity,
   SafeAreaView,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
-import { Button, Menu, Provider, TextInput } from "react-native-paper";
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import { Button, Menu, Provider, TextInput } from 'react-native-paper';
+import axios from 'axios';
+import { DatePickerModal } from 'react-native-paper-dates';
 
 const AddEvent = () => {
-  
   const navigation = useNavigation();
   function navigateBack() {
     navigation.goBack();
   }
 
-  const [visible, setVisible] = React.useState("");
-  const [title, setTitle] = React.useState("");
-  const [description, setDescription] = React.useState("");
-  const [location, setLocation] = useState("");
-  const [numGuests, setNumGuests] = useState("");
-  const [category, setCategory] = useState("");
+  const [visible, setVisible] = React.useState('');
+  const [title, setTitle] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [location, setLocation] = useState('');
+  const [numGuests, setNumGuests] = useState('');
+  const [category, setCategory] = useState('');
   const [day, setDay] = useState(new Date());
   const [startTime, setStartTime] = useState(new Date());
   const [endTime, setEndTime] = useState();
@@ -43,19 +44,19 @@ const AddEvent = () => {
     end
   ) => {
     if (
-      title == "" ||
-      description == "" ||
-      guests == "" ||
-      category == "" ||
-      day == "" ||
-      start == "" ||
-      end == ""
+      title == '' ||
+      description == '' ||
+      guests == '' ||
+      category == '' ||
+      day == '' ||
+      start == '' ||
+      end == ''
     ) {
       setAddMessage();
-      setAddErrorMessage("Please fill in all fields");
+      setAddErrorMessage('Please fill in all fields');
     } else {
       axios
-        .post("https://togethrgroup1.herokuapp.com/api/addevent", {
+        .post('https://togethrgroup1.herokuapp.com/api/addevent', {
           EventName: title,
           EventDescription: description,
           EventLocation: location,
@@ -71,136 +72,148 @@ const AddEvent = () => {
           (response) => {
             console.log(response);
             setAddErrorMessage();
-            setAddMessage("Your event was posted!");
+            setAddMessage('Your event was posted!');
           },
           (error) => {
             console.log(error);
             setAddMessage();
-            setAddErrorMessage("Something went wrong. Try again.");
+            setAddErrorMessage('Something went wrong. Try again.');
           }
         );
     }
   };
 
   return (
-    <Provider>
-      <View style={styles.container}>
-        <View style={styles.closeButton}>
-          <TouchableOpacity onPress={() => navigateBack()}>
-            <Ionicons name="close" size={30} color="back" />
-          </TouchableOpacity>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Provider>
+        <View style={styles.container}>
+          <View style={styles.closeButton}>
+            <TouchableOpacity onPress={() => {}}>
+              <Ionicons name="close" size={30} color="back" />
+            </TouchableOpacity>
+          </View>
+
+          <Text style={styles.verticalDivider}></Text>
+          <Text h1 style={styles.title}>
+            Create Activity
+          </Text>
+          <Text style={styles.verticalDivider}></Text>
+
+          <TextInput
+            style={{ alignSelf: 'stretch' }}
+            label="Title"
+            value={title}
+            mode="outlined"
+            onChangeText={(title) => setTitle(title)}
+          />
+
+          <TextInput
+            style={{ alignSelf: 'stretch' }}
+            label="Description"
+            value={description}
+            mode="outlined"
+            onChangeText={(descrip) => setDescription(descrip)}
+          />
+
+          <TextInput
+            style={{ alignSelf: 'stretch' }}
+            label="Location"
+            value={location}
+            mode="outlined"
+            onChangeText={(location) => setLocation(location)}
+          />
+
+          <TextInput
+            style={{ alignSelf: 'stretch' }}
+            label="Number of Guests"
+            value={numGuests}
+            mode="outlined"
+            onChangeText={(numGuests) => setNumGuests(numGuests)}
+          />
+
+          <Menu
+            visible={visible}
+            onDismiss={closeMenu}
+            anchor={
+              <Button
+                mode="outlined"
+                style={{ alignSelf: '' }}
+                onFocus={openMenu}
+              >Category</Button>
+            }>
+            <Menu.Item title={'Movie'} />
+            <Menu.Item title={'Music'} />
+            <Menu.Item title={'Sports'} />
+            <Menu.Item title={'Outdoors'} />
+            <Menu.Item title={'Food'} />
+            <Menu.Item title={'Animals'} />
+            <Menu.Item title={'Beauty'} />
+            <Menu.Item title={'Gaming'} />
+            <Menu.Item title={'Sight Seeing'} />
+            <Menu.Item title={'Technology'} />
+            <Menu.Item title={'DIY'} />
+            <Menu.Item title={'Travel'} />
+            <Menu.Item title={'Performing Arts'} />
+            <Menu.Item title={'Fine Arts'} />
+            <Menu.Item title={'Cars'} />
+            <Menu.Item title={'Photography'} />
+            <Menu.Item title={'Lifestyle'} />
+            <Menu.Item title={'Shopping'} />
+          </Menu>
+
+          <TextInput
+            style={{ alignSelf: 'stretch' }}
+            label="Date"
+            value={numGuests}
+            mode="outlined"
+            onChangeText={(numGuests) => setNumGuests(numGuests)}
+          />
+
+          <Button
+            onPress={() =>
+              postEvent(
+                title,
+                description,
+                location,
+                numGuests,
+                category,
+                day,
+                startTime,
+                endTime
+              )
+            }
+            style={styles.postButton}>
+            POST
+          </Button>
         </View>
-
-        <Text style={styles.verticalDivider}></Text>
-        <Text h1 style={styles.title}>
-          Create Activity
-        </Text>
-        <Text style={styles.verticalDivider}></Text>
-
-        <TextInput
-          style={{ alignSelf: "stretch" }}
-          label="Title"
-          value={title}
-          mode="outlined"
-          onChangeText={(title) => setTitle(title)}
-        />
-
-        <Text style={styles.inputDivider}></Text>
-
-        <TextInput
-          style={{ alignSelf: "stretch" }}
-          label="Description"
-          value={description}
-          mode="outlined"
-          onChangeText={(descrip) => setDescription(descrip)}
-        />
-
-        <TextInput
-          style={{ alignSelf: "stretch" }}
-          label="Location"
-          value={location}
-          mode="outlined"
-          onChangeText={(location) => setLocation(location)}
-        />
-
-        <TextInput
-          style={{ alignSelf: "stretch" }}
-          label="Number of Guests"
-          value={numGuests}
-          mode="outlined"
-          onChangeText={(numGuests) => setNumGuests(numGuests)}
-        />
-
-        <Menu
-          visible={visible}
-          onDismiss={closeMenu}
-          anchor={<Button onPress={openMenu}>Category</Button>}
-        >
-          <Menu.Item title={"Movie"} />
-          <Menu.Item title={"Music"} />
-          <Menu.Item title={"Sports"} />
-          <Menu.Item title={"Outdoors"} />
-          <Menu.Item title={"Food"} />
-          <Menu.Item title={"Animals"} />
-          <Menu.Item title={"Beauty"} />
-          <Menu.Item title={"Gaming"} />
-          <Menu.Item title={"Sight Seeing"} />
-          <Menu.Item title={"Technology"} />
-          <Menu.Item title={"DIY"} />
-          <Menu.Item title={"Travel"} />
-          <Menu.Item title={"Performing Arts"} />
-          <Menu.Item title={"Fine Arts"} />
-          <Menu.Item title={"Cars"} />
-          <Menu.Item title={"Photography"} />
-          <Menu.Item title={"Lifestyle"} />
-          <Menu.Item title={"Shopping"} />
-        </Menu>
-
-        <Button
-          onPress={() =>
-            postEvent(
-              title,
-              description,
-              location,
-              numGuests,
-              category,
-              day,
-              startTime,
-              endTime
-            )
-          }
-          style={styles.postButton}
-        >
-          POST
-        </Button>
-      </View>
-    </Provider>
+      </Provider>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    margin: 25,
+  },
   title: {
-    fontSize: 60,
-    fontFamily: "Comfortaa_400Regular",
-    alignSelf: "center",
+    fontSize: 30,
+    fontFamily: 'Comfortaa_400Regular',
+    alignSelf: 'center',
   },
   input: {
     width: 500,
   },
   closeButton: {
-    alignSelf: "flex-end",
+    alignSelf: 'flex-end',
   },
   center: {
     flex: 1,
-    alignContent: "center",
-    alignSelf: "center",
-  },
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    margin: 25,
+    alignContent: 'center',
+    alignSelf: 'center',
   },
   verticalDivider: {
     height: 10,
@@ -209,26 +222,26 @@ const styles = StyleSheet.create({
     height: 20,
   },
   fixToText: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   buttonDivider: {
     width: 10,
   },
   postButton: {
-    backgroundColor: "black",
-    borderColor: "black",
-    width: 500,
+    backgroundColor: 'black',
+    // width: 'stretch',
     height: 50,
-    borderWidth: 3,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 5,
+    //borderWidth: 3,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center',
+    //borderRadius: 5,
   },
   postButtonText: {
     fontSize: 18,
-    color: "white",
-    fontFamily: "Roboto_500Medium",
+    color: 'white',
+    fontFamily: 'Roboto_500Medium',
   },
 });
 
