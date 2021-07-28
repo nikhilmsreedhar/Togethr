9bfbd7238f0ed017ee261c72da8fcc4b98a34a46
Michael Negre <michael.nehgrei@gmail.com>
2021-07-27 20:01:26 -0400

    CardStack fix?

--- frontend/desktop/components/EventCard.js
+++ frontend/desktop/components/EventCard.js
@@ -1,65 +1,67 @@
-import React from 'react';
-import {View, StyleSheet, Text,Dimensions} from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, Dimensions } from "react-native";
 
-
-const SCREEN_HEIGHT = Dimensions.get('window').height
-const SCREEN_WIDTH = Dimensions.get('window').width
-const BUFFER = (Dimensions.get('window').width) * 0.2
-const HBUFFER = (Dimensions.get('window').height) * 0.3
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const BUFFER = Dimensions.get("window").width * 0.2;
+const HBUFFER = Dimensions.get("window").height * 0.3;
 
 const EventCard = ({
-    title, 
-    description,
-    location,
-    startDate,
-    endDate,
-    guests,
-    attendees,
-    tag
+  title,
+  description,
+  location,
+  startDate,
+  endDate,
+  guests,
+  attendees,
+  tag,
 }) => {
-    return (
-      <View style={styles.containerCardItem}>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.description}>{description}</Text>
-        <View style={{flexDirection: 'row'}}>
+  return (
+    <View style={styles.containerCardItem}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <View style={{ flexDirection: "row" }}>
         <Text style={styles.info}>{startDate}</Text>
         <Text style={styles.info}> - </Text>
         <Text style={styles.info}>{endDate}</Text>
-        </View>
-        {/* <Text style={styles.info}>{attendees.length}/{guests}</Text> */}
-        <Text style={styles.info}>{attendees.map(attendees=><Text>{attendees}</Text>)}</Text>
       </View>
-   );
-}
- 
+      {/* <Text style={styles.info}>{attendees.length}/{guests}</Text> */}
+      <View style={styles.info}>
+        {attendees.map((attendee) => 
+          {return <Text style={styles.info}>{attendee}</Text>}
+        )}
+      </View>
+    </View>
+  );
+};
 
-  const styles = StyleSheet.create({
-    containerCardItem: {
+const styles = StyleSheet.create({
+  containerCardItem: {
     height: SCREEN_HEIGHT - HBUFFER,
     width: SCREEN_WIDTH - BUFFER,
     paddingVertical: 50,
-		backgroundColor: 'white',
-		borderRadius: 8,
-		alignItems: "center",
-    justifyContent: 'center',
-		shadowOpacity: 0.05,
-		shadowRadius: 10,
-		shadowColor: 'black',
-		shadowOffset: { height: 0, width: 0 }
-    }, 
-    title: {
-      fontSize: 60,
-      fontWeight: 'bold',
-      margin: 10,
-    },
-    description: {
-      fontSize: 40,
-      margin: 10,
-    },
-    info: {
-      fontSize: 30,
-      margin: 10,
-    }
+    backgroundColor: "white",
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    shadowColor: "black",
+    shadowOffset: { height: 0, width: 0 },
+  },
+  title: {
+    fontSize: 60,
+    fontWeight: "bold",
+    margin: 10,
+  },
+  description: {
+    fontSize: 40,
+    margin: 10,
+  },
+  info: {
+    fontSize: 30,
+    margin: 10,
+  },
 });
-  
+
 export default EventCard;
--- frontend/desktop/pages/Explore.js
+++ frontend/desktop/pages/Explore.js
@@ -1,126 +1,139 @@
-import React, { useEffect } from 'react';
-import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
-import { useIsFocused } from '@react-navigation/native'
-import { Ionicons } from '@expo/vector-icons';
-import CardStack, { Card } from 'react-native-card-stack-swiper';
-import EventCard from '../components/EventCard';
-import Data, { push } from '../assets/data.js'
-import NavigationBar from '../components/NavigationBar';
-import axios from 'axios';
+import React, { useEffect, useCallback } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { useFocusEffect, useIsFocused } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import CardStack, { Card } from "react-native-card-stack-swiper";
+import EventCard from "../components/EventCard";
+import Data, { push } from "../assets/data.js";
+import NavigationBar from "../components/NavigationBar";
+import axios from "axios";
 
 function Explore() {
 
   //retrieve user tags for POST to API
-  var _ud = localStorage.getItem('user_data');
-  var ud = JSON.parse(_ud);
-  var etags = ud.tags;
+  const getUserTags = () => {
+    const _ud = localStorage.getItem("user_data");
+    const ud = JSON.parse(_ud);
+    return ud.tags;
+  };
 
-  // every time isFocused is updated, useEffect calls getEventsData
   useEffect(() => {
-    getEventsData();
-    console.log("Called");
-    console.log(eventData);
-  } , [isFocused])
+    const userTags = getUserTags();
+    getEventsData(userTags);
+
+  }, [isFocused]);
+
+  
+  //returns true or false depending on if page is focused or not
+  const isFocused = useIsFocused();
+
+  // useFocusEffect should run every time the Explore page is focused
+  // useFocusEffect(() => {
+  //   getEventsData();
+  //   console.log("called");
+  //   console.log(eventData);
+  // });
 
   //for use by the swiper
   const t = {};
 
-  const isFocused = useIsFocused()
   const [eventData, setEventsData] = React.useState([]);
 
-  async function getEventsData() {
-    await axios.post('https://togethrgroup1.herokuapp.com/api/retrieveevents', {
-        Tags: etags
+  async function getEventsData(userTags) {
+    await axios
+      .post("https://togethrgroup1.herokuapp.com/api/retrieveevents", {
+        Tags: userTags,
       })
-      .then((response) => {
-        console.log(response);
-        setEventsData([...response.data]);
-        console.log("Updated eventData");
-      }, (error) => {
-        console.log(error);
-        alert("Something went wrong!");
-      });
+      .then(
+        (response) => {
+          console.log(response);
+          setEventsData([...response.data]);
+          console.log("Updated eventData");
+        },
+        (error) => {
+          console.log(error);
+          alert("Something went wrong!");
+        }
+      );
   }
 
-
   return (
-    <SafeAreaView style={{flex: 1}}>
-    <NavigationBar/>
-    <View style={styles.page}>
-      <CardStack 
-        disableTopSwipe = {true}
-        disableBottomSwipe = {true}
-        disableLeftSwipe = {true}
-        disableRightSwipe = {true}
-        style={styles.content}
-        ref={swiper => {t.swiper = swiper }}
-        renderNoMoreCards={() => <Text style={{ fontSize: 18, color: 'gray' }}>No more events to display</Text>}
-        onSwipedLeft={() => 
-          
-          alert('swiped left')
-        }
-        onSwipedRight={() => 
-          alert('swiped right')
-        }
-        onSwipedBottom={() => 
-          
-          alert('swiped down')
-      }>
-        {eventData.map((item, index) => (
-          <Card key={index}>
-            <EventCard
-              title={item.EventName}
-              description={item.EventDescription}
-              startDate={item.StartDate}
-              endDate={item.EndDate}
-              attendees={item.Attendees}
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationBar />
+      <View style={styles.page}>
+        <CardStack
+          disableTopSwipe
+          disableBottomSwipe
+          disableLeftSwipe
+          disableRightSwipe
+          style={styles.content}
+          ref={(swiper) => {
+            t.swiper = swiper;
+          }}
+          renderNoMoreCards={() => (
+            <Text style={{ fontSize: 18, color: "gray" }}>
+              No more events to display
+            </Text>
+          )}
+          onSwipedLeft={() => alert("swiped left")}
+          onSwipedRight={() => alert("swiped right")}
+          onSwipedBottom={() => alert("swiped down")}
+        >
+          {eventData.map((item, index) => (
+            <Card key={index}>
+              <EventCard
+                title={item.EventName}
+                description={item.EventDescription}
+                startDate={item.StartDate}
+                endDate={item.EndDate}
+                attendees={item.Attendees}
               />
-          </Card>
-         ))}
-      </CardStack>
+            </Card>
+          ))}
+        </CardStack>
 
-      <View style={{flexDirection:'row'}}> 
-      <TouchableOpacity 
-        onPress={() => t.swiper.swipeLeft()}>
-       <Ionicons name="close-circle"  size={50} color="back" />
-      </TouchableOpacity>
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity onPress={() => t.swiper.swipeLeft()}>
+            <Ionicons name="close-circle" size={50} color="back" />
+          </TouchableOpacity>
 
-      <Text style={styles.buttonSpace}></Text>
+          <View style={styles.buttonSpace} />
 
-      <TouchableOpacity 
-        onPress={() => t.swiper.swipeBottom()}>
-       <Ionicons name="heart-outline"  size={45} color="back" />
-      </TouchableOpacity>
+          <TouchableOpacity onPress={() => t.swiper.swipeBottom()}>
+            <Ionicons name="heart-outline" size={45} color="back" />
+          </TouchableOpacity>
 
-      <Text style={styles.buttonSpace}></Text>
+          <View style={styles.buttonSpace} />
 
-      <TouchableOpacity 
-        onPress={() => t.swiper.swipeRight()}>
-       <Ionicons name="checkmark-circle"  size={50} color="back" />
-      </TouchableOpacity>
-
-    </View>
-    </View>
+          <TouchableOpacity onPress={() => t.swiper.swipeRight()}>
+            <Ionicons name="checkmark-circle" size={50} color="back" />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
-
 }
-
 
 const styles = StyleSheet.create({
-  content:{
-   flex: 5,
-   alignItems: 'center',
-   justifyContent: 'center',
- },
- page:{
-  flex: 1, 
-  justifyContent: 'center', 
-  alignItems: 'center', 
-  paddingBottom: 50,
-},
-buttonSpace:{
-  width: 20
-}
-})
+  content: {
+    flex: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 50,
+  },
+  buttonSpace: {
+    width: 20,
+  },
+});
 export default Explore;
